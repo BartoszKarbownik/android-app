25e15fbeff2e211f71672bcf990f1706333d956b
Bartosz Karbownik <bartosz.karbownik01@gmail.com>
2024-06-26 22:37:45 +0200

    fragments

---
+++ ToDoListApp/app/src/main/java/com/example/todolistapp/TaskListFragment.kt
@@ -0,0 +1,70 @@
+package com.example.todolistapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todolistapp.R
+import com.example.todolistapp.databinding.FragmentTaskListBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TaskListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+
+class TaskListFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var _binding: FragmentTaskListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentTaskListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.taskRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        // TODO: Set up RecyclerView adapter
+
+        binding.addTaskButton.setOnClickListener {
+            findNavController().navigate(R.id.action_taskListFragment_to_addTaskFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+//
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment TaskListFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            TaskListFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+}
